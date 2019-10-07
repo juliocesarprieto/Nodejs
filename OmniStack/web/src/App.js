26e3e57ae46a8e28addc2477ee19a546a12f3242
Julio Velazquez <julio.velazquez@indt.org.br>
2019-10-07 17:44:12 -0400

    Added login and routes

--- OmniStack/web/src/App.js
+++ OmniStack/web/src/App.js
@@ -1,30 +1,21 @@
 import React from 'react';
+
 import './App.css';
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
+  
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form action="">
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            id="email"
-            type="email"
-            placeholder="Seu melhor e-mail"
-          />
-
-          <button className="btn " type="submit">Entrar</button>
-        </form>
+        <Routes />        
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- OmniStack/web/src/services/api.js
+++ OmniStack/web/src/services/api.js
@@ -1,1 +1,7 @@
-i
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3333'
+});
+
+export default api;
